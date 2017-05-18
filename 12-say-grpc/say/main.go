c2447c446c26871401e9950fb1db88ecf6c2cd29
Francesc Campoy <campoy@golang.org>
2017-05-17 17:44:03 -0700

    Episode 12: a Text Speech server with gRPC and Kubernetes (#5)
    * initial commit of grpc-say
    
    Change-Id: I3790b9b4faadaa1d91822b21676364ffb6991f47
    
    * proto api
    
    Change-Id: I77a11f62c058a440c6a18956c48c7bf0ab1758f3
    
    * end to end, not working
    
    Change-Id: I100ae639ebfa6c392f7328de1c9a517d40020eb2
    
    * end to end working locally
    
    Change-Id: I70bd17f9c9e68c279b386e72ebdf320208cd2049
    
    * deploy to kubernetes
    
    Change-Id: Iec17e221bac3f0ed55a5998e45cc6153ab2309a4
    
    * expose IP
    
    Change-Id: If0cb3f56f32f37edcdb24af12b4cff8c601a7b8d
    
    * before episode starts
    
    Change-Id: I292c3d335fd50d523e03468c60b228ce62d3ed88
    
    * Dockerfile
    
    Change-Id: I134e9650ac7b0eadd290dd43b39711a5fc8a1eb8
    
    * simple backend working
    
    Change-Id: I93416fa01c3c56d2fa0ab4262f35d2e1d2441ed9
    
    * grpc api
    
    Change-Id: Ic91eb4bc294a24da5f47d4d1c7e21d4c26e5a045
    
    * grpc server dummy
    
    Change-Id: Id021af9c2dc78f483ce2d66da9181deb111d5bb6
    
    * client implemented
    
    Change-Id: I0cafc8fb1d638019118d488c44c4766b70891489
    
    * all working
    
    Change-Id: I63130ef6e60d800147fb2d03badd1f253947c43c
    
    * everything works
    
    Change-Id: I6b81fdde85e823f57f901ac5fb40c2f3c660364f
    
    * prepare to push
    
    Change-Id: I56d0843e4c62dbde1e4d8eb2fc57867354ab49b4
    
    * update autogenerated Go code
    
    Change-Id: I8da0c8cc368431a615cf07b0dee3d6abeffea36d
    
    * update copyright add lines at EOF
    
    Change-Id: I45c6cc87c79d7c4e4d108dfbd0c146560a92e4b2
    
    * update main README
    
    Change-Id: I26505ab95645659c0fb8ac79fbd450101cf349bb

---
+++ 12-say-grpc/say/main.go
@@ -0,0 +1,56 @@
+// Copyright 2107 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to writing, software distributed
+// under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/campoy/justforfunc/12-say-grpc/api"
+)
+
+func main() {
+	backend := flag.String("b", "localhost:8080", "address of the say backend")
+	output := flag.String("o", "output.wav", "wav file where the output will be written")
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Printf("usage:\n\t%s \"text to speak\"\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	conn, err := grpc.Dial(*backend, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect to %s: %v", *backend, err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTextToSpeechClient(conn)
+
+	text := &pb.Text{Text: flag.Arg(0)}
+	res, err := client.Say(context.Background(), text)
+	if err != nil {
+		log.Fatalf("could not say %s: %v", text.Text, err)
+	}
+
+	if err := ioutil.WriteFile(*output, res.Audio, 0666); err != nil {
+		log.Fatalf("could not write to %s: %v", *output, err)
+	}
+}
--- README.md
+++ README.md
@@ -17,6 +17,7 @@ series [#justforfunc](http://youtube.com/c/justforfunc).
 - 9: The Context Package ([video](https://www.youtube.com/watch?v=LSzR0VEraWw&index=1&list=PL64wiCrrxh4Jisi7OcCJIUpguV_f5jGnZ) and [source code](09-context))
 - 10: Implementing The Context Package ([video](https://youtu.be/8M90t0KvEDY?list=PL64wiCrrxh4Jisi7OcCJIUpguV_f5jGnZ) and [source code](10-contextimpl))
 - 11: Review of an IRC package's API ([video](https://www.youtube.com/watch?v=nhElL62BSn0&index=12&list=PL64wiCrrxh4Jisi7OcCJIUpguV_f5jGnZ) and [source code](https://github.com/davidjpeacock/shelbot/pull/36))
+- 12: A Text to Speech server with gRPC and Kubernetes ([video](https://www.youtube.com/watch?v=XaMr--wAuSI&index=12&list=PL64wiCrrxh4Jisi7OcCJIUpguV_f5jGnZ) and [source code](12-say-grpc))
 
 ### Have an idea?
 
