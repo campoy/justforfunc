ae199a4133c7fa3596fbe83ff803c4fefd83a77f
Francesc Campoy <campoy@google.com>
2017-07-24 08:32:35 -0700

    episode 16
    Change-Id: Ic05857f6ec0198a69aa61fb687a50f60369d68f5

---
+++ 16-testing/README.md
@@ -0,0 +1,21 @@
+# justforfunc 16: unit testing HTTP servers
+
+Let's cover the basics of unit testing in Go and then show how you can test
+[http.HandlerFunc](https://golang.org/pkg/net/http#HandlerFunc) and
+[http.Handler](https://golang.org/pkg/net/http#Handler).
+
+You'll learn how to use "testing", including subtests and examples, and "net/http/httptest" with ResponseRecorder and Server.
+
+<div style="text-align:center">
+    <a href="https://www.youtube.com/watch?v=hVFEV-ieeew&feature=youtu.be&list=PL6">
+        <img src="https://img.youtube.com/vi/hVFEV-ieeew/0.jpg" alt="justforfunc 16: unit testing HTTP servers">
+        <p>justforfunc 16: unit testing HTTP servers</p>
+    </a>
+</div>
+
+References:
+
+- testing: https://golang.org/pkg/testing/
+- net/http/httptest: https://golang.org/pkg/net/http/httptest
+- Testable Examples in Go: https://blog.golang.org/examples
+- source code: https://github.com/campoy/justforfunc/tree/master/16-logpipe
--- 16-testing/sum/sum_test.go
+++ 16-testing/sum/sum_test.go
@@ -1,9 +1,20 @@
-package sum_test
+// Copyright 2017 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to writing, software distributed
+// under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sum
 
 import (
 	"testing"
-
-	"github.com/campoy/justforfunc/16-testing/sum"
 )
 
 func TestInts(t *testing.T) {
@@ -19,7 +30,7 @@ func TestInts(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			s := sum.Ints(tc.numbers...)
+			s := Ints(tc.numbers...)
 			if s != tc.sum {
 				t.Fatalf("sum of %v should be %v; got %v", tc.name, tc.sum, s)
 			}
