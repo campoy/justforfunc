80421424e7d52df8cb9ca1561544302a5206c9c4
Francesc Campoy <campoy@google.com>
2017-07-24 08:32:35 -0700

    episode 16
    Change-Id: Ic05857f6ec0198a69aa61fb687a50f60369d68f5

--- 16-testing/webserver/main_test.go
+++ 16-testing/webserver/main_test.go
@@ -1,7 +1,21 @@
+// Copyright 2017 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to writing, software distributed
+// under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -61,5 +75,32 @@ func TestDoubleHandler(t *testing.T) {
 				t.Fatalf("expected double to be %v; got %v", tc.double, d)
 			}
 		})
+	}
+}
+
+func TestRouting(t *testing.T) {
+	srv := httptest.NewServer(handler())
+	defer srv.Close()
+
+	res, err := http.Get(fmt.Sprintf("%s/double?v=2", srv.URL))
+	if err != nil {
+		t.Fatalf("could not send GET request: %v", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("could not read response: %v", err)
+	}
+
+	d, err := strconv.Atoi(string(bytes.TrimSpace(b)))
+	if err != nil {
+		t.Fatalf("expected an integer; got %s", b)
+	}
+	if d != 4 {
+		t.Fatalf("expected double to be 4; got %v", d)
 	}
 }
