732a4e07d3042cbb57a27f52a631ac48cc49e4b9
Francesc Campoy <campoy@golang.org>
2017-09-11 17:17:43 +1000

    Pipes (#28)
    * add episode 16 in the main README
    
    Change-Id: Ief7ec292ec0053c9c7354f9c0a6968fd2d033bd5
    
    * intro done
    
    Change-Id: I256d380270a3d9de163a95e0960158604844df66
    
    * added LICENSE
    
    Change-Id: Iaeec114f7929d8337f5c60cc1df52c4176d9fa8e
    
    * before writer
    
    Change-Id: I4638ddfd49588fca22c7f31c74415bd5cbb165ac
    
    * episode 19 is ready
    
    Change-Id: I85ad8c261a36c660a745ec83f4b018ce1a340d22
    
    * remove go 1.7:
    
    Change-Id: Ieba834efe0d7092340c28f6fcbad3bdc1b6da7c4

---
+++ 19-pipes/basic/pipes/main.go
@@ -0,0 +1,37 @@
+// Copyright 2017 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to writing, software distributed
+// under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	pr, pw := io.Pipe()
+
+	go func() {
+		defer pw.Close()
+		_, err := fmt.Fprintln(pw, "hello")
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	_, err := io.Copy(os.Stdout, pr)
+	if err != nil {
+		panic(err)
+	}
+}
