fcf24b8d1823b6e807ad5a4e9f48ad8bb43a2606
Francesc Campoy <campoy@golang.org>
2017-11-13 18:30:14 -0800

    added statz with text only
    Change-Id: Idbb453567b565e6d39ced4cd69eeacbf5a671da8

--- 23-plot/main.go
+++ 23-plot/main.go
@@ -5,17 +5,26 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"sync"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	http.HandleFunc("/", rootHandler)
+	var s server
+
+	http.HandleFunc("/", s.root)
+	http.HandleFunc("/statz", s.statz)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+type server struct {
+	data []time.Duration
+	sync.RWMutex
+}
+
+func (s *server) root(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -25,4 +34,20 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	d := time.Duration(x) * time.Millisecond
 	time.Sleep(d)
 	fmt.Fprintln(w, "slept for", d)
+
+	s.Lock()
+	s.data = append(s.data, d)
+	if len(s.data) > 1000 {
+		s.data = s.data[len(s.data)-1000:]
+	}
+	s.Unlock()
+}
+
+func (s *server) statz(w http.ResponseWriter, r *http.Request) {
+	s.RLock()
+	defer s.RUnlock()
+
+	for _, d := range s.data {
+		fmt.Fprintln(w, d)
+	}
 }
