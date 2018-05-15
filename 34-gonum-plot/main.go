a27a4f632c7d0d42810f376e162117280cb4c011
Francesc Campoy <campoy@golang.org>
2018-05-15 09:00:55 -0700

    Episode 34: Plotting Data with gonum/plot (#65)
    * reading data.txt
    
    * writing empty graph
    
    * write data and fake linear regression line
    
    * adding READMEs for episode 34

---
+++ 34-gonum-plot/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"image/color"
+	"log"
+	"os"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg/draw"
+)
+
+func main() {
+	xys, err := readData("data.txt")
+	if err != nil {
+		log.Fatalf("could not read data.txt: %v", err)
+	}
+	_ = xys
+
+	err = plotData("out.png", xys)
+	if err != nil {
+		log.Fatalf("could not plot data: %v", err)
+	}
+}
+
+type xy struct{ x, y float64 }
+
+func readData(path string) (plotter.XYs, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var xys plotter.XYs
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		var x, y float64
+		_, err := fmt.Sscanf(s.Text(), "%f,%f", &x, &y)
+		if err != nil {
+			log.Printf("discarding bad data point %q: %v", s.Text(), err)
+		}
+		xys = append(xys, struct{ X, Y float64 }{x, y})
+	}
+	if err := s.Err(); err != nil {
+		return nil, fmt.Errorf("could not scan: %v", err)
+	}
+	return xys, nil
+}
+
+func plotData(path string, xys plotter.XYs) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("could not create %s: %v", path, err)
+	}
+
+	p, err := plot.New()
+	if err != nil {
+		return fmt.Errorf("could not create plot: %v", err)
+	}
+
+	// create scatter with all data points
+	s, err := plotter.NewScatter(xys)
+	if err != nil {
+		return fmt.Errorf("could not create scatter: %v", err)
+	}
+	s.GlyphStyle.Shape = draw.CrossGlyph{}
+	s.Color = color.RGBA{R: 255, A: 255}
+	p.Add(s)
+
+	var x, c float64
+	x = 1.2
+	c = -3
+
+	// create fake linear regression result
+	l, err := plotter.NewLine(plotter.XYs{
+		{3, 3*x + c}, {20, 20*x + c},
+	})
+	if err != nil {
+		return fmt.Errorf("could not create line: %v", err)
+	}
+	p.Add(l)
+
+	wt, err := p.WriterTo(256, 256, "png")
+	if err != nil {
+		return fmt.Errorf("could not create writer: %v", err)
+	}
+	_, err = wt.WriteTo(f)
+	if err != nil {
+		return fmt.Errorf("could not write to %s: %v", path, err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("could not close %s: %v", path, err)
+	}
+	return nil
+}
