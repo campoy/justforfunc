42a3c9b7b2970d5b757eb70186257d5544889a47
Francesc Campoy <campoy@golang.org>
2018-07-11 16:45:37 +0200

    ready to merge

---
+++ 37-sync-pool/README.md
@@ -0,0 +1,16 @@
+# sync.Pool from the pool
+
+It's summer and I love hanging out by the pool ...
+so it's just logic that I would make an episode about sync.Pool!
+
+In this episode we learn how to measure how many memory allocations
+a program is performing and how to reduce them thanks to sync.Pool,
+as well as how to identify which piece of your code is
+allocating the memory thanks to pprof.
+
+<div style="text-align:center">
+    <a href="https://youtu.be/rfXSrgIGrKo">
+        <img src="https://img.youtube.com/vi/rfXSrgIGrKo/0.jpg" alt="justforfunc #37: sync.Pool from the pool">
+        <p>justforfunc #37: sync.Pool from the pool</p>
+    </a>
+</div>
--- 37-sync-pool/main.go
+++ 37-sync-pool/main.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/src-d/go-github/github"
 )
 
 func main() {
@@ -14,12 +14,23 @@ func main() {
 	logrus.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+var prPool = sync.Pool{
+	New: func() interface{} { return new(pullRequest) },
+}
+
+type pullRequest struct {
+	PullRequest struct{ ID int } `json:"pull_request"`
+}
+
 func handle(w http.ResponseWriter, r *http.Request) {
-	var data github.PullRequestEvent
-	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+	data := prPool.Get().(*pullRequest)
+	defer prPool.Put(data)
+	data.PullRequest.ID = 0
+
+	if err := json.NewDecoder(r.Body).Decode(data); err != nil {
 		logrus.Errorf("could not decode request: %v", err)
 		http.Error(w, "could not decode request", http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "pull request id: %d", *data.PullRequest.ID)
+	fmt.Fprintf(w, "pull request id: %d", data.PullRequest.ID)
 }
